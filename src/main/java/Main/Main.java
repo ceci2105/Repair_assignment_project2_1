9ea8592ee643041fc6830d3dbae57de2964ea314
Tiimeeeyy <timur.schmidt@student.maastrichtuniversity.nl>
2024-09-11 12:06:47 +0200

    Added javaFX dependency. Adjusted main class to fit JavaFX.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,15 @@
 package Main;
 
-public class Main{
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
