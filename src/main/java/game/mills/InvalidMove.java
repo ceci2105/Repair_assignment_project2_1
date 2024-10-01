260c211d7ff54298bb810ba69a8350aad17d05d6
i6319956 <v.cauwegoeres@student.maastrichtuniversity.nl>
2024-10-01 12:50:31 +0200

    Javadoc and commenting for InvalidMove class

--- src/main/java/game/mills/InvalidMove.java
+++ src/main/java/game/mills/InvalidMove.java
@@ -1,6 +1,17 @@
 package game.mills;
 
+/**
+ * The InvalidMove class represents an exception that is thrown when a player attempts 
+ * to make an invalid move in the game.
+ * It extends the RuntimeException class and provides a custom message indicating the reason for the invalid move.
+ */
 public class InvalidMove extends RuntimeException {
+
+    /**
+     * Constructs a new InvalidMove exception with the specified detail message.
+     *
+     * @param message the detail message explaining why the move is invalid.
+     */
     public InvalidMove(String message) {
         super(message);
     }
