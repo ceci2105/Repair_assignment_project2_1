5d7945af7b7c691316cd1e5cac8caf6dc065a759
i6319956 <v.cauwegoeres@student.maastrichtuniversity.nl>
2024-10-01 12:37:48 +0200

    Javadoc for player class

--- src/main/java/game/mills/Player.java
+++ src/main/java/game/mills/Player.java
@@ -2,12 +2,51 @@ package game.mills;
 
 import javafx.scene.paint.Color;
 
+/**
+ * The Player interface defines the essential moves and parameters for any player of the game.
+ * It includes methods for retrieving player information, managing the number of stones a player has, and tracking the game state for the player.
+ */
 public interface Player {
+    /**
+     * Gets the name of the player.
+     * 
+     * @return the name of the player as a String.
+     */
     String getName();
+
+    /**
+     * Gets the color representing the player's pieces.
+     * 
+     * @return the color of the player's pieces.
+     */
     Color getColor();
+
+    /**
+     * Gets the number of stones the player still has to place on the board.
+     * 
+     * @return the number of stones left to place.
+     */
     int getStonesToPlace();
+
+    /**
+     * Gets the number of stones the player currently has on the board.
+     * 
+     * @return the number of stones on the board.
+     */
     int getStonesOnBoard();
+
+    /**
+     * Decreases the number of stones the player has left to place by one.
+     */
     void decrementStonesToPlace();
+
+    /**
+     * Increases the number of stones the player has on the board by one.
+     */
     void incrementStonesOnBoard();
+
+    /**
+     * Decreases the number of stones the player has on the board by one.
+     */
     void decrementStonesOnBoard();
 }
